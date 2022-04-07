f08f8ba15d7f9e26fa4cc4d892ec6ea8b7ac2b20
chlarizuh <chlarizza.torres.cics@ust.edu.ph>
2022-04-07 15:54:39 +0800

    chla contacts

--- frontend/src/components/home/Contacts.js
+++ frontend/src/components/home/Contacts.js
@@ -6,46 +6,84 @@ const Contacts = () => {
   return (
     <Fragment>
       <body>
-        <div class="container">
-          <div class="content">
-            <div class="left-side">
-              <div class="address details">
-                <i class="fas fa-map-marker-alt"></i>
-                <div class="topic">Address</div>
-                <div class="text-one">Surkhet, NP12</div>
-                <div class="text-two">Birendranagar 06</div>
+        <div class="container-contact100">
+          <div class="wrap-contact100">
+            <form class="contact100-form validate-form">
+             <span class="contact100-form-title"> Send Us A Message</span>
+              <label class="label-input100" for="first-name"> Tell us your name</label>
+                <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Type your first name"> 
+                  <input id="first-name" class="input100" type="text" name="first-name" placeholder="First name"></input>
+                  <span class="focus-input100"></span>
+                </div>
+                <div class="wrap-input100 rs2-wrap-input100 validate-input" data-validate="Type your last name"> 
+                  <input class="input100" type="text" name="last-name" placeholder='Last name'></input>
+                  <span class ="focus-input100"></span>
+                </div>
+
+              <label class="label-input100" for="email">Enter your email</label>
+                <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                  <input id="email" class="input100" type="text" name="email" placeholder="Ex. gandachla@true.com"></input>
+                  <span class="focus-input100"></span>
+                </div>
+
+              <label class="label-input100" for="phone">Enter your phone number</label>
+                <div class="wrap-input100">
+                  <input id="phone" class="input100" type="text" name="phone" placeholder="0918076247"></input>
+                  <span class="focus-input100"></span>
+                </div>
+
+              <label class="label-input100" for="message">Message</label>
+                <div class="wrap-input100 validate-input" data-validate="Messageis required">
+                  <textarea id="message" class="input100" name="message" placeholder='Write us a message'></textarea>
+                  <span class="focus-input100"></span>
               </div>
-              <div class="phone details">
-                <i class="fas fa-phone-alt"></i>
-                <div class="topic">Phone</div>
-                <div class="text-one">+0098 9893 5647</div>
-                <div class="text-two">+0096 3434 5678</div>
+
+              <div class="container-contact100-form-btn">
+                <button class="contact100-form-btn">Send Message</button>
               </div>
-              <div class="email details">
-                <i class="fas fa-envelope"></i>
-                <div class="topic">Email</div>
-                <div class="text-one">codinglab@gmail.com</div>
-                <div class="text-two">info.codinglab@gmail.com</div>
-              </div>
-            </div>
-            <div class="right-side">
-              <div class="topic-text">Send us a message</div>
-              <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
-            <form action="#">
-              <div class="input-box">
-                
-              </div>
-              <div class="input-box">
-              
-              </div>
-              <div class="input-box message-box">
-                
-              </div>
-              <div class="button">
-                
-              </div>
+
             </form>
-          </div>
+
+            <div class="contact100-more flex-col-x-m" >
+              <div class ="flex-w size1 p-b-47">
+                <div clas="txt1 p-r-25">
+                  <span class="fas fa-map-marker-alt">
+                  </span>
+                </div>
+                <div class="flex-col size2">
+                  <span class="txt1 p-b-20">Address</span>
+                  <span class="txt2">Nueve Ecija</span>
+                </div>
+              </div>
+
+              <div class ="dis-flex size1 p-b-47">
+                <div clas="txt1 p-r-25">
+                  <span class="fas fa-phone-alt"></span>
+                </div> 
+                <div class="flex-col size2">
+                  <span class="txt1 p-b-20"> Phone</span>
+                  <span class="txt2">09560472527</span>
+                </div>
+              </div>
+
+              <div class ="dis-flex size1 p-b-47">
+                <div clas="txt1 p-r-25">
+                  <span class="fas fa-envelope p-r-25"></span>
+                </div> 
+                <div class="flex-col size2">
+                  <span class="txtemail p-b-20">E-mail</span>
+                  <span class="txt2email">rgbaby@gmail.com</span>
+                </div>
+              </div>
+
+             
+              
+
+            </div>
+              
+            
+           
+
           </div>
         </div>
 
@@ -57,3 +95,22 @@ const Contacts = () => {
 }
 
 export default Contacts
+
+{/* <div class="address details">
+<i class="fas fa-map-marker-alt"></i>
+<div class="topic">Address</div>
+<div class="text-one">Surkhet, NP12</div>
+<div class="text-two">Birendranagar 06</div>
+</div>
+<div class="phone details">
+<i class="fas fa-phone-alt"></i>
+<div class="topic">Phone</div>
+<div class="text-one">+0098 9893 5647</div>
+<div class="text-two">+0096 3434 5678</div>
+</div>
+<div class="email details">
+<i class="fas fa-envelope"></i>
+<div class="topic">Email</div>
+<div class="text-one">codinglab@gmail.com</div>
+<div class="text-two">info.codinglab@gmail.com</div>
+</div> */}
